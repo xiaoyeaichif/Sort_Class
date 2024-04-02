97badd7eebf5ab31d8cc7337706392c29288b98d
xiaoyeaichif <1125035561@qq.com>
2024-04-02 19:35:42 +0800

    quick

--- Sort_Class/04快速排序.cpp
+++ Sort_Class/04快速排序.cpp
@@ -2,35 +2,35 @@
 #include <vector>
 
 // 快速排序函数
-void quickSort(std::vector<int>& arr, int low, int high) {
-    // 当低位索引小于高位索引时进行排序
-    if (low < high) {
-        // 选择中间元素作为枢纽元素
-        int pivot = arr[(low + high) / 2];
-        int left = low;
-        int right = high;
-
-        // 将数组划分为两部分，使左边部分的元素小于枢纽元素，右边部分的元素大于枢纽元素
-        while (left<= right) {
-            // 在左边找到第一个大于等于枢纽元素的元素
-            while (arr[left] < pivot) {
-                ++left;
+int Parition(std::vector<int>& arr, int low, int high) {
+        // 选择第一个元素作为枢纽元素
+        int pivot = arr[low];
+        while (low < high)
+        {
+            //找到第一个小于pivot的元素
+            while (low < high && arr[high] >= pivot)
+            {
+                high--;
             }
-            // 在右边找到第一个小于等于枢纽元素的元素
-            while (arr[right] > pivot) {
-                --right;
+            arr[low] = arr[high];
+            //找到第一个大于pivot的元素下标
+            while (low < high && arr[low] <= pivot)
+            {
+                low++;
             }
-            // 如果找到了合适的元素，则交换它们
-            if (left <= right) {
-                std::swap(arr[left], arr[right]);
-                left++;
-                right--;
-            }
+            arr[high] = arr[low];
         }
-
-        // 递归排序左右两部分
-        quickSort(arr, low, right);
-        quickSort(arr, left, high);
+        //最后low == high之后,这个空位由pivot填补
+        arr[low] = pivot;
+        return low;
+}
+void Quick_sort(std::vector<int>& arr, int low, int high)
+{
+    if (low < high)
+    {
+        int Pivot = Parition(arr, low, high);
+        Quick_sort(arr, low, Pivot - 1);
+        Quick_sort(arr, Pivot + 1, high);
     }
 }
 
@@ -39,7 +39,7 @@ int main() {
     std::vector<int> arr = { 12, 11, 13, 5, 6, 7 };
     int n = arr.size();
     // 调用快速排序函数
-    quickSort(arr, 0, n - 1);
+    Quick_sort(arr, 0, n - 1);
     // 输出排序后的结果
     std::cout << "Sorted array: ";
     for (int num : arr) {
